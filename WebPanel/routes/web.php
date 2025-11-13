554e3d76a2c399ab197fc4547779d33f4e0ef64e
Jason <jasonvanl2019@gmail.com>
2025-11-13 17:59:50 +0100

    Updated code for more links

--- WebPanel/routes/web.php
+++ WebPanel/routes/web.php
@@ -4,7 +4,7 @@ use Livewire\Volt\Volt;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('/{code}')
-    ->where(['code' => '[A-Za-z0-9]{5}'])
+    ->where(['code' => '[A-Za-z0-9]{10}'])
     ->group(function () {
         Volt::route('/', 'server')
         ->name('server.index');
