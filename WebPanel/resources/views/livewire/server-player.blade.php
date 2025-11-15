7b14c9c3efd85c6a4f5c40315c00abfd49dee793
Jason <jasonvanl2019@gmail.com>
2025-11-15 10:32:07 +0100

    Added a back to players btn

--- WebPanel/resources/views/livewire/server-player.blade.php
+++ WebPanel/resources/views/livewire/server-player.blade.php
@@ -49,94 +49,105 @@ mount(function ($code, $uuid) {
 
 ?>
 
-<div class="player__container">
+<div>
     @vite('resources/sass/pages/player.scss')
     @section('title', $code . " - " . $player['name'] . " (" . $player['uuid'] . ")")
 
     <livewire:components.header :code="$code" :page="'players'"/>
 
-    <x-datacontainer>
-        <div class="player">
+    <div class="back-btn">
+        <a href="{{ route('server.players', $code) }}">
+            <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                 stroke-linecap="round" stroke-linejoin="round">
+                <path d="M15 18l-6-6 6-6"></path>
+            </svg>
+            Back to Players
+        </a>
+    </div>
 
-            <div class="player__header">
-                <img src="https://mc-heads.net/avatar/{{ $player['uuid'] }}/80" alt="{{ $player['name'] }}">
-                <div>
-                    <h1>{{ $player['name'] }}</h1>
-                    <p class="uuid">{{ $player['uuid'] }}</p>
+    <div class="player__container">
+        <x-datacontainer>
+            <div class="player">
+                <div class="player__header">
+                    <img src="https://mc-heads.net/avatar/{{ $player['uuid'] }}/80" alt="{{ $player['name'] }}">
+                    <div>
+                        <h1>{{ $player['name'] }}</h1>
+                        <p class="uuid">{{ $player['uuid'] }}</p>
+                    </div>
                 </div>
-            </div>
 
-            <div class="player__section-title">Player Stats</div>
-            <table class="player__table">
-                <tbody>
-                <tr>
-                    <th>Timestamp</th>
-                    <td>{{ $player['stats']['timestamp'] }}</td>
-                </tr>
-                <tr>
-                    <th>Kills</th>
-                    <td>{{ $player['stats']['kills'] }}</td>
-                </tr>
-                <tr>
-                    <th>Deaths</th>
-                    <td>{{ $player['stats']['deaths'] }}</td>
-                </tr>
-                <tr>
-                    <th>Playtime (ms)</th>
-                    <td>{{ number_format($player['stats']['playtime_ms']) }}</td>
-                </tr>
-
-                <tr>
-                    <th>Status</th>
-                    <td>
-                        @if($player['stats']['is_online'])
-                            <span class="status-dot online"></span> Online
-                        @else
-                            <span class="status-dot offline"></span> Offline
-                        @endif
-                    </td>
-                </tr>
-
-                <tr>
-                    <th>Last Login</th>
-                    <td>{{ $player['stats']['last_login'] }}</td>
-                </tr>
-                <tr>
-                    <th>Current World</th>
-                    <td>{{ $player['stats']['current_world'] }}</td>
-                </tr>
-
-                <tr>
-                    <th>Position</th>
-                    <td>
-                        X: {{ $player['stats']['position']['x'] }} <br>
-                        Y: {{ $player['stats']['position']['y'] }} <br>
-                        Z: {{ $player['stats']['position']['z'] }}
-                    </td>
-                </tr>
-                </tbody>
-            </table>
-
-            <div class="player__section-title">Sessions</div>
-            <table class="player__table">
-                <thead>
-                <tr>
-                    <th>Timestamp</th>
-                    <th>Type</th>
-                    <th>Server</th>
-                </tr>
-                </thead>
-                <tbody>
-                @foreach($player['sessions'] as $session)
+                <div class="player__section-title">Player Stats</div>
+                <table class="player__table">
+                    <tbody>
                     <tr>
-                        <td>{{ $session['timestamp'] }}</td>
-                        <td>{{ ucfirst($session['session_type']) }}</td>
-                        <td>{{ $session['server_id'] }}</td>
+                        <th>Timestamp</th>
+                        <td>{{ $player['stats']['timestamp'] }}</td>
                     </tr>
-                @endforeach
-                </tbody>
-            </table>
+                    <tr>
+                        <th>Kills</th>
+                        <td>{{ $player['stats']['kills'] }}</td>
+                    </tr>
+                    <tr>
+                        <th>Deaths</th>
+                        <td>{{ $player['stats']['deaths'] }}</td>
+                    </tr>
+                    <tr>
+                        <th>Playtime (ms)</th>
+                        <td>{{ number_format($player['stats']['playtime_ms']) }}</td>
+                    </tr>
 
-        </div>
-    </x-datacontainer>
+                    <tr>
+                        <th>Status</th>
+                        <td>
+                            @if($player['stats']['is_online'])
+                                <span class="status-dot online"></span> Online
+                            @else
+                                <span class="status-dot offline"></span> Offline
+                            @endif
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <th>Last Login</th>
+                        <td>{{ $player['stats']['last_login'] }}</td>
+                    </tr>
+                    <tr>
+                        <th>Current World</th>
+                        <td>{{ $player['stats']['current_world'] }}</td>
+                    </tr>
+
+                    <tr>
+                        <th>Position</th>
+                        <td>
+                            X: {{ $player['stats']['position']['x'] }} <br>
+                            Y: {{ $player['stats']['position']['y'] }} <br>
+                            Z: {{ $player['stats']['position']['z'] }}
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+
+                <div class="player__section-title">Sessions</div>
+                <table class="player__table">
+                    <thead>
+                    <tr>
+                        <th>Timestamp</th>
+                        <th>Type</th>
+                        <th>Server</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @foreach($player['sessions'] as $session)
+                        <tr>
+                            <td>{{ $session['timestamp'] }}</td>
+                            <td>{{ ucfirst($session['session_type']) }}</td>
+                            <td>{{ $session['server_id'] }}</td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+
+            </div>
+        </x-datacontainer>
+    </div>
 </div>
